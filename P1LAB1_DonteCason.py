479d3eb180c5045e9bb4606c4c69b7fa30aa3646
FTCCDonte <casond2608@student.faytechcc.edu>
2024-06-20 13:58:06 -0400

    Add files via upload
    1st Submisson

---
+++ P1LAB1_DonteCason.py
@@ -0,0 +1,9 @@
+# Donte Cason
+# June 18, 2024
+# P1LAB1
+# Creating a python for a grade.
+
+first_Donte = input("Enter your first name:Donte")
+last_name = input("Enter your last name:Cason")
+print("hello "+ first_name,last_name+"!")
+print("hello "+ first_name,last_name+"!"Welcome to CTI-110)
